18777744ca360dbdf71e285ee1c88793877ac014
sage <hello@sageelliott.com>
2022-11-24 22:05:52 -0800

    map functions to call in comments

---
+++ run.py
@@ -0,0 +1,18 @@
+'''
+File for running churn_library functions
+'''
+
+# %%Import data
+
+
+# %% Perform EDA
+
+# %% encoder_helper
+
+# %% perform_feature_engineering
+
+# %% classification_report_image
+
+# %% feature_importance_plot
+
+# %% train_models
