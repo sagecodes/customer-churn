80eeb8022635faa1ad8da5c998ae44261820c0eb
sage <hello@sageelliott.com>
2023-03-11 15:04:26 -0800

    test model training

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -5,6 +5,8 @@ import churn_library as cls
 from churn_library import (import_data,
 							perform_eda,
 							encoder_helper,
+							perform_feature_engineering,
+							train_models
 							)
 
 logging.basicConfig(
@@ -73,18 +75,58 @@ def test_encoder_helper(encoder_helper):
 			logging.error("Testing encoder_helper: missing column")
 			raise err
 		
-	
+
 
 def test_perform_feature_engineering(perform_feature_engineering):
 	'''
 	test perform_feature_engineering
 	'''
+	# df = import_data("./data/bank_data.csv")
+
+	# train_test_values = perform_feature_engineering(df)
+
+	# #loop through the train_test_values from keep list
+
 
 
 def test_train_models(train_models):
 	'''
 	test train_models
 	'''
+	df = import_data("./data/bank_data.csv")
+
+	category_lst = ['Gender',
+                'Education_Level',
+                'Marital_Status',
+                'Income_Category',
+                'Card_Category']
+
+	keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
+             'Total_Relationship_Count', 'Months_Inactive_12_mon',
+             'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+             'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+             'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
+             'Income_Category_Churn', 'Card_Category_Churn']
+
+	target = 'Churn'
+
+	encoded_df = encoder_helper(df, category_lst, target)
+
+	train_X, test_X, train_y, test_y = perform_feature_engineering(encoded_df, keep_cols, target)
+
+	train_models(train_X, test_X, train_y, test_y)
+
+
+	try:
+		assert os.path.exists("./models/logistic_model_train.pkl")
+		assert os.path.exists("./models/rfc_model_train.pkl")
+		logging.info("Testing train_models - All models saved: SUCCESS")
+	except AssertionError as err:
+		logging.error("Testing train_models - Models saved: file(s) not found")
+		raise err
+	
+
 
 
 if __name__ == "__main__":
@@ -92,6 +134,9 @@ if __name__ == "__main__":
 	# df = import_data("./data/bank_data.csv")
 	test_eda()
 	test_encoder_helper(encoder_helper)
+	# test_perform_feature_engineering(perform_feature_engineering)
+	test_train_models(train_models)
+
 
 
 
